0058444315c8b4aae592a6487322e0a81d7eb102
Krishna Kumar <krishnakumar091@yahoo.com>
2026-04-06 01:25:41 +0530

    Fix formatting and add S3 endpoint configuration in aws.py

--- services/api/app/core/aws.py
+++ services/api/app/core/aws.py
@@ -1,9 +1,17 @@
 # aws.py
-import boto3
 import os
 
-REGION = os.getenv("AWS_REGION", "ap-south-1")
+import boto3
+from botocore.config import Config
 
-s3 = boto3.client("s3", region_name=REGION)
+REGION = os.getenv("AWS_REGION", "ap-south-1")
+S3_ENDPOINT = f"https://s3.{REGION}.amazonaws.com"
+
+s3 = boto3.client(
+    "s3",
+    region_name=REGION,
+    endpoint_url=S3_ENDPOINT,
+    config=Config(signature_version="s3v4", s3={"addressing_style": "virtual"})
+)
 dynamodb = boto3.resource("dynamodb", region_name=REGION)
 sqs = boto3.client("sqs", region_name=REGION)
